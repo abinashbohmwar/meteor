9a3e5b7359b8b9df0d44d6f7c2205f5e3435c3bb
Abinash Bohmwar <abinash.bohmwar@mailinator.com>
2015-10-29 22:35:22 +0100

    Introduced helpers and repetitions

--- notification.js
+++ notification.js
@@ -1,12 +1,21 @@
 if (Meteor.isClient) {
+		  
+	  
+	Template.body.helpers({
+		jobs: [
+			{ title: "Job 1" },
+			{ title: "Job 2" },
+			{ title: "Job 3" }
+		]
+	});
+	  
+  
 }
 
 if (Meteor.isServer) {
 	
-  Meteor.startup(function () {
-	  
-	  
-	  
-  });
+	Meteor.startup(function () {
+
+	});
 
 }
